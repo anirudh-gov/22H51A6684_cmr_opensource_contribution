2881e882014a9deecb7dc55df5e0a496116a469f
Govardhana Anirudh <anirudhgovardhana@gmail.com>
2024-06-19 12:36:36 +0530

    Adding all ML Models

---
+++ ML_ALL_MODELS/app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC,SVR
+
+
+print("Anirudh")
